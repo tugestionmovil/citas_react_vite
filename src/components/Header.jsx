8f8f2a695152081cc2a4fd5acb94411f40f86bb4
interelsy <interelsy@gmail.com>
2023-01-26 17:13:22 -0400

    Dia a dia avanzo un poc mas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   
   return (
     <>
-      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes {""}<span className="text-indigo-600">Veterinaria</span></h1>
+      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes {""} con Github {' '}<span className="text-indigo-600">Veterinaria</span></h1>
     </>
   )
 }
